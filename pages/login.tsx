a9b70e877b56d1fd90baad680fc51ab5cd0b904d
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-11-14 09:08:33 +0900

    login fix

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,7 +6,7 @@ const Line = () => {
       <section className="w-1/3 h-2/3 shadow-md flex items-center justify-center flex-col">
         <h2>Line Messaging API</h2>
         <p className="mb-2 text-center">sample text</p>
-        <button className="btn-blue">Let's Start!!</button>
+        <button className="btn-blue">Lets Start!!</button>
       </section>
     </Layout>
   );
